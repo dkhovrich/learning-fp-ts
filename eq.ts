ce9e8254f9347d82dc2c02eb68b5b9f9bc4386f1
Dmitriy Khovrich <dkhovrich@readdle.com>
2020-09-21 22:17:33 +0300

    eq, ord

---
+++ eq.ts
@@ -0,0 +1,11 @@
+import { contramap, eqNumber } from "fp-ts/Eq";
+
+type User = {
+    userId: number;
+    name: string;
+}
+
+const eqUser = contramap<number, User>(user => user.userId)(eqNumber);
+
+eqUser.equals({ userId: 1, name: 'Giulio' }, { userId: 1, name: 'Giulio Canti' });
+eqUser.equals({ userId: 1, name: 'Giulio' }, { userId: 2, name: 'Giulio' });
