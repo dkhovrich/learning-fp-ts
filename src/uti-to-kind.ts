8d8dd91171672785be91236ab810699c97de8520
Dmitriy Khovrich <dkhovrich@readdle.com>
2020-12-20 23:04:04 +0200

    mappableMap

--- src/uti-to-kind.ts
+++ src/uti-to-kind.ts
@@ -22,8 +22,16 @@ type URIS2 = keyof URItoKind2<unknown, unknown>;
 type Kind<F extends URIS, A> = URItoKind<A>[F];
 type Kind2<F extends URIS2, A, B> = URItoKind2<A, B>[F];
 
+type MyArray = Kind<"Array", string>;
+type MySet = Kind<"Set", string>;
+type MyMap = Kind2<"Map", string, number>;
+
 interface Mappable<F extends URIS> {
     readonly map: <A, B>(f: (a: A) => B) => (as: Kind<F, A>) => Kind<F, B>
+}
+
+interface Mappable2<F extends URIS2> {
+    readonly map: <A, B, C>(f: (a: B) => C) => (as: Kind2<F, A, B>) => Kind2<F, A, C>
 }
 
 const mappableArray: Mappable<"Array"> = {
@@ -34,6 +42,10 @@ const mappableSet: Mappable<"Set"> = {
     map: f => as => new Set(Array.from(as).map(f))
 }
 
+const mappableMap: Mappable2<"Map"> = {
+    map: f => as => new Map(Array.from(as.entries()).map(([key, value]) => [key, f(value)] as const))
+}
+
 const multiply = (a: number) => (b: number): number => a * b;
 
 const r1 = mappableArray.map(multiply(2))([1, 2, 3, 4, 5]);
@@ -41,3 +53,7 @@ console.log(r1);
 
 const r2 = mappableSet.map(multiply(2))(new Set([1, 2, 3, 4, 5]));
 console.log(r2);
+
+const m = new Map<string, number>([["One", 1], ["Two", 2], ["Three", 3]]);
+const r3 = mappableMap.map(multiply(2))(m);
+console.log(r3);
